49131b4d0643c3c281f09ba913198eae87c36c5e
Axel Falk <axel.falk@hotmail.com>
2021-02-03 17:02:16 +0100

    fixed letter typos

--- src/Game.java
+++ src/Game.java
@@ -10,13 +10,13 @@ public class Game {
 		keyboard = new Scanner(System.in);
 		locations = new ArrayList<>();
 		
-		Location start = new Location("Hemma", "Du Ã¤r ensam hemma i din lilla etta.\nTill maten slinker bÃ¥de en och tvÃ¥ bira ner och du bÃ¶rjar bli sugen pÃ¥ att dra vidare. \nOvven Ã¤r knuten, vart ska du fortsÃ¤tta kvÃ¤llen?", 0);
-		Location utekrök = new OutdoorsArea("Utekrök", "Du kommer till vallamassivet. Det Ã¤r skymning och burkar ligger slÃ¤ngda i varje buske. \nEn grupp festerister spelar hÃ¶g musik och skriker ramsor. \nEn kall bris kyler dig genom den tunna sommarjackan.", 30, "soligt");
-		Location korrefest = new Room("Korrefest", "Du anlÃ¤nder till en korridorsfest i Ryd. \nSom vanligt Ã¤r det alldeles fÃ¶r mÃ¥nga personer pÃ¥ alldeles fÃ¶r fÃ¥ kvadratmeter. \nSpriten flÃ¶dar och i hÃ¶gtalarna hÃ¶rs schlager.", 20, 20);
-		Location flamman = new Room("Flamman", "Du kommer till slut ner i till flamman. I underjorden Ã¤r det mÃ¶rkt och dunkelt. \nStudenterna kÃ¤mpar fÃ¶r att verka nyktra framfÃ¶r vakterna. StÃ¤mningen Ã¤r pÃ¥ topp.", 200, 80);
-		Location kårallen = new Room("Kårallen", "Ã¤ntligen inne pÃ¥ KÃ¥rallen! Det Ã¤r Ã¥rets sista kravall och det stora dansgolvet Ã¤r proppfullt med taggade studenter. \nPÃ¥ scenen spelar Asap Rocky och publiken Ã¤r som galen. \nRÃ¶k-och-stÃ¶k-tÃ¤ltet har aldrig varit sÃ¥ fullt.", 1200, 400);
-		Location donken = new Room("Donken", "Slutligen anlÃ¤nder du dit du velat komma hela kvÃ¤llen, din Ã¤lskade restaurang Gyllene MÃ¥sen. \nDu Ã¤ter tvÃ¥ dubbelcheese och skryter om att du fick en gratis McFlurry Daim med din stora andel Mcpoints \nsom visar pÃ¥ att du kommer hit alldeles fÃ¶r ofta.", 1250, 100);
-		Location efterfest = new Room("Efterfest", "KvÃ¤llen tar en ovÃ¤ntad vÃ¤ndning, du hamnar pÃ¥ efterfest i ryd. \nNÃ¥gon ligger dÃ¤ckad, ett par sitter o strular i soffan. \nVar detta verkligen en bra ide?", 15, 25);
+		Location start = new Location("Hemma", "Du är ensam hemma i din lilla etta.\nTill maten slinker både en och två bira ner och du börjar bli sugen på att dra vidare. \nOvven är knuten, vart ska du fortsätta kvällen?", 0);
+		Location utekrök = new OutdoorsArea("Utekrök", "Du kommer till vallamassivet. Det är skymning och burkar ligger slängda i varje buske. \nEn grupp festerister spelar hög musik och skriker ramsor. \nEn kall bris kyler dig genom den tunna sommarjackan.", 30, "soligt");
+		Location korrefest = new Room("Korrefest", "Du anländer till en korridorsfest i Ryd. \nSom vanligt är det alldeles för många personer på alldeles för få kvadratmeter. \nSpriten flödar och i högtalarna hörs schlager.", 20, 20);
+		Location flamman = new Room("Flamman", "Du kommer till slut ner i till flamman. I underjorden är det mörkt och dunkelt. \nStudenterna kämpar för att verka nyktra framför vakterna. Stämningen är på topp.", 200, 80);
+		Location kårallen = new Room("Kårallen", "äntligen inne på Kårallen! Det är årets sista kravall och det stora dansgolvet är proppfullt med taggade studenter. \nPå scenen spelar Asap Rocky och publiken är som galen. \nRök-och-stök-tältet har aldrig varit så fullt.", 1200, 400);
+		Location donken = new Room("Donken", "Slutligen anländer du dit du velat komma hela kvällen, din älskade restaurang Gyllene Måsen. \nDu äter två dubbelcheese och skryter om att du fick en gratis McFlurry Daim med din stora andel Mcpoints \nsom visar på att du kommer hit alldeles för ofta.", 1250, 100);
+		Location efterfest = new Room("Efterfest", "Kvällen tar en oväntad vändning, du hamnar på efterfest i ryd. \nNågon ligger däckad, ett par sitter o strular i soffan. \nVar detta verkligen en bra ide?", 15, 25);
 		
 		start.setAlternatives(new Location[]{null, null, utekrök, korrefest});
 		utekrök.setAlternatives(new Location[]{null, flamman, kårallen, korrefest});
@@ -38,17 +38,17 @@ public class Game {
 	public void run() {
 		String name;
 
-		System.out.println("VÃ¤lkommen till en utekvÃ¤ll i LinkÃ¶ping!\nVad heter du?");
+		System.out.println("VÃ¤lkommen till en utekväll i Linköping!\nVad heter du?");
 		name = keyboard.nextLine();
 		player = new Player(name, locations.get(0));
 		System.out.println("Hej " + name
-				+ ", din stÃ¶kiga utekvÃ¤ll ska precis bÃ¶rja.\nDu kan rÃ¶ra dig i de fyra riktiningarna east/west/north/south.\n(Tips: Du kan anvÃ¤nda kommandot [help] nÃ¤r som helst fÃ¶r att fÃ¥ hjÃ¤lp)");
+				+ ", din stökiga utekväll ska precis börja.\nDu kan röra dig i de fyra riktiningarna east/west/north/south.\n(Tips: Du kan använda kommandot [help] när som helst för att få hjälp)");
 
 		while (true) {
 			String command;
 
 			player.getLocation().describeYourself();
-			System.out.println("Vart vill du rÃ¶ra dig?");
+			System.out.println("Vart vill du röra dig?");
 			command = keyboard.nextLine();
 			player.doCommand(command);
 		}
